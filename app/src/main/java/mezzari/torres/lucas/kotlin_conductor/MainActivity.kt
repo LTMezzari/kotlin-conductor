c1192024c9d234bed891341a0da985f2ba4ea4ad
Lucas Torres Mezzari <lucas.mezzari1@gmail.com>
2019-07-09 19:41:16 -0300

    Initital Commit

---
+++ app/src/main/java/mezzari/torres/lucas/kotlin_conductor/MainActivity.kt
@@ -0,0 +1,12 @@
+package mezzari.torres.lucas.kotlin_conductor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
